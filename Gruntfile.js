9e59ed43e753dc308c217272d4b49a48601dbe4f
Aidan Jalali <aidanjalali@gmail.com>
2014-08-22 17:27:00 +0100

    updating images, adding sass/grunt setupg

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        sass: {
+            prod: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'www/css/style.css': 'sass/style.scss'
+                }
+            }
+        },
+        
+        watch: {
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass:prod']
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.registerTask('default', ['sass']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-yudish-mssl
+Yudish's MSSL site
 ===========
 
-yudish-mssl
+Styles for the page are generated with [SASS](http://sass-lang.com/) and compiled to the css folder. To edit the styles, you will need NodeJS and Grunt.
+
+1. Install [Node](http://nodejs.org/)
+2. Install Bower. In your terminal/Git Bash, CD to this project's root directory and type:
+
+    npm install
+    
+3. You're good to go! To compile the SASS, type `grunt sass`, or to watch for changes to the SASS file and automatically recompile, type `grunt watch`.
